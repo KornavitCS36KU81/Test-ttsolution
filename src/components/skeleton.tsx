edae2ea58a6859e804adabacfaed02b44e678554
kornavit <kornavit.p@ku.th>
2026-03-22 20:06:25 +0700

    change skeleton loading

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,11 +1,10 @@
 export default function Skeleton() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">รายการที่คุณต้องทำ</h1>
+    <div className="pt-7.5">
       <div className="animate-pulse space-y-8 mt-8">
         <div className="h-24 md:h-8 md:flex md:justify-between space-y-4 md:space-y-0">
-          <div className="bg-gray-300 rounded-xl h-8 md:w-36" />
-          <div className="bg-gray-300 rounded-xl h-8 md:w-16" />
+          <div className="bg-gray-300 rounded-xl h-8 md:w-1/2" />
+          <div className="bg-gray-300 rounded-xl h-8 md:block-0" />
         </div>
 
         <div className="space-y-3">
@@ -14,14 +13,13 @@ export default function Skeleton() {
               <div className="h-8 bg-gray-300 rounded-xl w-24" />
               <div className="h-20 bg-gray-300 rounded-xl" />
             </div>
-            <div className="h-8 w-10 bg-gray-300 rounded-xl"/>
           </div>
 
-          <div className="flex h-8 justify-between">
-            <div className="bg-gray-300 rounded-xl w-24" />
-            <div className="flex space-x-4">
-              <div className="bg-gray-300 rounded-full w-24" />
-              <div className="bg-gray-300 rounded-full w-16" />
+          <div className="flex flex-col md:flex-row h-20 md:h-8 justify-between">
+            <div className="bg-gray-300 rounded-xl h-8 w-full md:w-24" />
+            <div className="flex justify-between space-x-2 space-y-2">
+              <div className="bg-gray-300 rounded-full h-8 w-24" />
+              <div className="bg-gray-300 rounded-full h-8 w-16" />
             </div>
           </div>
         </div>
